914b8c200c844344a6f5c8d82673cead7c554271
StanislavJS <stasyk55@gmail.com>
2025-03-31 13:24:39 +0300

    index.html

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,15 @@
-function getElementWidth(content, padding, border) {
-  // Видаляємо "px" з кожного параметра і перетворюємо на числа
-  const contentWidth = parseFloat(content); // Перетворюємо content на число
-  const paddingWidth = parseFloat(padding); // Перетворюємо padding на число
-  const borderWidth = parseFloat(border); // Перетворюємо border на число
-
-  // Загальна ширина = контент + 2 * (падінг + бордер)
-  const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
-
-  return totalWidth;
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+  return filteredNumbers;
 }
 
-// Перевірка коректності роботи функції
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
